6240942aef64c2248839fc3af85d29e4e785d94b
Jeason <me@jeasonstudio.cn>
2021-12-28 15:16:54 +0800

    chore: change prod public path

--- web/.umirc.ts
+++ web/.umirc.ts
@@ -4,8 +4,8 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
+  // publicPath for gh-pages
+  publicPath: process.env.NODE_ENV === 'production' ? '/cryptofish/' : '/',
 });
