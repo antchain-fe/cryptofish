51dedf66d0654962fc38b56bf13b31b55daa81ee
Jeason <me@jeasonstudio.cn>
2022-01-07 20:15:01 +0800

    fix: logo placeholder flash

--- web/src/pages/components/Logo.tsx
+++ web/src/pages/components/Logo.tsx
@@ -4,14 +4,35 @@ import { useInterval } from 'ahooks';
 import { Canvas } from '@/components/Canvas';
 import { Attribute, getRandomAttribute } from '@/common/attribute';
 
-const BorderedCanvas = styled(Canvas)`
-  border-radius: 5px;
+import img1 from '@/assets/logo/1.png';
+import img2 from '@/assets/logo/2.png';
+import img3 from '@/assets/logo/3.png';
+import img4 from '@/assets/logo/4.png';
+import img5 from '@/assets/logo/5.png';
+import img6 from '@/assets/logo/6.png';
+import img7 from '@/assets/logo/7.png';
+
+const images = [img1, img2, img3, img4, img5, img6, img7];
+
+const LogoContainer = styled.div`
+  width: 204px;
+  height: 204px;
+  & img {
+    border-radius: 5px;
+    width: 100%;
+  }
 `;
 
 export const Logo: React.FC<unknown> = () => {
-  const [attribute, setAttribtue] = React.useState<Record<Attribute, string>>(getRandomAttribute());
+  const [index, setIndex] = React.useState(0);
+
   useInterval(() => {
-    setAttribtue(getRandomAttribute());
+    setIndex(index + 1);
   }, 1000);
-  return <BorderedCanvas ratio={6} attribute={attribute} />;
+
+  return (
+    <LogoContainer>
+      <img src={images[index % images.length]} alt="logo-placeholder" />
+    </LogoContainer>
+  );
 };
