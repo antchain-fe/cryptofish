f46785be53e3db8fe913b4edb26b0b25438ab8db
Jeason <me@jeasonstudio.cn>
2022-01-08 13:26:38 +0800

    release: prod version

--- contract/config/myfish.js
+++ contract/config/myfish.js
@@ -1,7 +1,7 @@
 module.exports = {
   contract: {
-    // deploy tx hash: 0xe61a417f792954f68e9be4f22cdd252fa17ea2acdf1757483d87cbc406fb0afc
-    name: 'cryptofish-release-0108',
+    // deploy tx hash: 0x7bab5ee9f8d690cf944e84d7d5027ee35edabf732184f00f08b511bfcfdcfe61
+    name: 'cryptofish',
     version: 1,
   },
 };
